a04da62f5a20c1b572a6bc1f5d9bd7cabe219c8c
Rajesh270712 <rajeshbagul2001@gmail.com>
2022-07-08 12:00:15 +0530

    complete

---
+++ main.js
@@ -0,0 +1,9 @@
+const add = require("./add");
+const divide = require("./divide");
+const multiply = require("./multiply");
+const subtract = require("./subtract");
+
+console.log(add(1, 2));
+console.log(divide(4, 2));
+console.log(multiply(4, 2));
+console.log(subtract(4, 2));
